894e656c011a2253e20731ba736d262448667e98
Pei-Yu <b20321115@gmail.com>
2022-08-15 17:03:10 +0800

    add vuetify plugin for vue3+vite

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,19 @@ import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <v-app>
+    <v-main>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" class="logo" alt="Vite logo" />
+        </a>
+        <a href="https://vuejs.org/" target="_blank">
+          <img src="/vue.svg" class="logo vue" alt="Vue logo" />
+        </a>
+      </div>
+      <HelloWorld msg="Vite + Vue" />
+    </v-main>
+  </v-app>
 </template>
 
 <style scoped>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -12,11 +12,7 @@ const count = ref(0)
   <h1>{{ msg }}</h1>
 
   <div class="card">
-    <button type="button" @click="count++">count is {{ count }}</button>
-    <p>
-      Edit
-      <code>components/HelloWorld.vue</code> to test HMR
-    </p>
+    <v-btn variant="outlined" @click="count++">count is {{ count }}</v-btn>
   </div>
 
   <p>
